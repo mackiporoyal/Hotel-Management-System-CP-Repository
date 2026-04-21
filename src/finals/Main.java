e83f17104e964fc18448b674264a068dc6fe12b5
mackiporoyal <engr.mackenzie@gmail.com>
2026-04-22 01:06:57 +0800

    po

--- src/finals/Main.java
+++ src/finals/Main.java
@@ -2,6 +2,6 @@ package finals;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello raya test hello raya");
+		System.out.println("hello raya test hello raya po");
 	}
 }
