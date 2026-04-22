bfd8cd31e9ebf52e88983470fb78e0b6d314ac3a
mackiporoyal <engr.mackenzie@gmail.com>
2026-04-23 00:39:53 +0800

    displayed string values of enum, used a return option that crated a string constructor for those enums. ready for create booking class

--- src/finals/Main.java
+++ src/finals/Main.java
@@ -1,11 +1,22 @@
 package finals;
-
+import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {		
-		Menu.eMenu[] menu = Menu.eMenu.values();
-		for (Menu.eMenu listMenu: Menu.eMenu.values()) {
-			System.out.println(listMenu);
+		Menu menu = new Menu();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("\t\t\t\t_________________WElCOME TO HMCS_________________");
+		menu.displayMenu();
+		System.out.print("\t\t\t\tENTER HERE: ");
+		int chooseMenu = scan.nextInt();
+		scan.nextLine();
+		
+		switch(chooseMenu) {
+		case 1: System.out.println("1");break;//goes to create booking class
+		case 2: System.out.println("2");break;//goes to search booking class
+		//etc option
+		default: System.out.println("Enter + //logic to count how many case/length of enum values// only!");
 		}
+		scan.close();
 	}
 }
- 
--- src/finals/Menu.java
+++ src/finals/Menu.java
@@ -2,14 +2,22 @@ package finals;
 
 public class Menu {
 	enum eMenu{
-		CREATE_BOOKING(""), SEARCH_BOOKING(""), CHECK_ROOM_AVAILABILITY(""), MANAGE_STAY_AND_BILLING(""), MASTER_RECORD(""); 
+		CREATE_BOOKING("1"), SEARCH_BOOKING("2"), CHECK_ROOM_AVAILABILITY("3"), MANAGE_STAY_AND_BILLING("4"), MASTER_RECORD("5"); 
 	 
 		private final String option;
 	    eMenu(String option) {
 	    	this.option = option; 
 	    }
+	    
 	    public String getMenu(){
 	    	return option;
 	    }
+	}
+
+	public void displayMenu() {
+		System.out.println("\t\t\t\t___________Choose what you wanna to do___________");
+		for(Menu.eMenu listMenu: Menu.eMenu.values()) {
+    		System.out.println("\t\t\t\t"+listMenu.option);
+    	}
 	}	
 }
