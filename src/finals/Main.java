edfc1a08bfef26f2494409d0a2f7966c0ecda33e
Charles <Charles@DESKTOP-HSNALKT>
2026-04-27 20:30:30 +0800

    ello

--- src/finals/Main.java
+++ src/finals/Main.java
@@ -20,6 +20,7 @@ public class Main {
 		//etc option
 		default: System.out.println("Enter + //logic to count how many case/length of enum values// only!");
 		}
-		scan.close();
+		scan.close(); // elo
+		
 	}
 }
