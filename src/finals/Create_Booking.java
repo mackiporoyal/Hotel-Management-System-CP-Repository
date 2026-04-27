a4de3bd0f2045f91b69a24e8d0f73733ab32c146
Charles <Charles@DESKTOP-HSNALKT>
2026-04-27 22:05:50 +0800

    CreateBooking & TextFileLogic

--- src/finals/Create_Booking.java
+++ src/finals/Create_Booking.java
@@ -1,6 +1,7 @@
 package finals;
 import java.nio.file.*;
-
+import java.nio.file.Files;
+import java.io.IOException;
 public class Create_Booking {
 
 	private int adultGuest;
@@ -10,5 +11,20 @@ public class Create_Booking {
 		this.adultGuest = newAdult;
 		this.childGuest = newChild;
 		this.name = newName;
+	}
+	public void savetoFile()
+	{
+		String bookingDets = String.format("Adults: %d, Children: %d%n, Name: %s", adultGuest, childGuest, name);
+		
+		try 
+		{
+			Files.write(Paths.get("booking.txt"),bookingDets.getBytes(),java.nio.file.StandardOpenOption.CREATE,java.nio.file.StandardOpenOption.APPEND);
+			System.out.println("Booking saved successfully!");
+
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error while saving booking" + e.getMessage());
+		}
 	}
 }
--- src/finals/TextFileLogic.java
+++ src/finals/TextFileLogic.java
@@ -1,13 +1,41 @@
 package finals;
 
 import java.nio.file.*;
-
-
+import java.util.Scanner;
+import java.io.IOException;
 public class TextFileLogic {
 
 	Path storagePath = Paths.get("HotelDatabase.txt");
 	Path fullPath = storagePath.toAbsolutePath();
 	public void d() {
-		System.out.println(this.fullPath);
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("How many adults? : ");
+		int adultnum = scan.nextInt();
+
+		System.out.println("How many Children? : ");
+		int childnum = scan.nextInt();
+		
+		
+		// room availability here
+		
+		System.out.println("Enter Guess Name: ");
+		String name = scan.nextLine();
+		
+		
+		try
+		{
+			Create_Booking bookDets = new Create_Booking(adultnum, childnum, name);
+			String bookdata = String.format("%d|%d%n|%s", adultnum, childnum, name);
+			Files.write(storagePath, bookdata.getBytes(),java.nio.file.StandardOpenOption.CREATE,java.nio.file.StandardOpenOption.APPEND);
+			
+			System.out.println("Booking Saved!");
+			
+			
+		}
+		catch (IOException e)
+		{
+			System.err.println("Error Found while saving: " + e.getMessage());
+		}
 	}
 }
