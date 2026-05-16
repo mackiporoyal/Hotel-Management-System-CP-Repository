9bb5f4f255a3a7d458033d208836f1ed563dd04d
Charles <Charles@DESKTOP-HSNALKT>
2026-05-17 01:51:19 +0800

    WIP

--- src/finals/UpdateStayBilling.java
+++ src/finals/UpdateStayBilling.java
@@ -1,10 +1,12 @@
 package finals;
 
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.io.*;
 
 public class UpdateStayBilling {
 	private String booknum;
@@ -37,8 +39,6 @@ public class UpdateStayBilling {
 		}
 	public void display ()
 	{
-		Path storageRPath = Paths.get("HotelDatabase.txt");
-		Path absolutePath = storageRPath.toAbsolutePath();
 			BufferedReader buffre;
 			String li;
 			String [] bn = null;
@@ -81,8 +81,58 @@ public class UpdateStayBilling {
 	String liney = ("\\|");
 	String [] col = liney.split("\\|");
 
+	
+
+		try {
+			BufferedWriter write = new BufferedWriter(new FileWriter("HotelDatabase.txt"));
+			write.write(changes);
+			
+		}
+	catch (Exception e)
+		{
 		
+		}
+		BufferedReader bofer;
+		String line;
+		String [] data = null;
+		try 
+		{
+			buffre = new BufferedReader(new FileReader("HotelDatabase.txt"));
+			while ((li = buffre.readLine()) != null)
+			{
+					String [] row = li.split("\\|");
+					if (row[0].trim().equals(booknum))
+					{
+						bn = row; 
+						break;
+					}
+			}
+			buffre.close();
+		}
+		catch (Exception e)
+		{
+			System.out.println("Error found while reading the textfile: " + e);
+		}
+		
+		//
+		for (head h : head.values())
+		{
+			System.out.print("     | " +h + " |  ");
+		}
+		System.out.println("\n");
+		System.out.printf("%12s %20s %17s %16s %19s %20s %18s %18s %18s %18s", bn);	
+		
+		System.out.println("\n");
+	
+	
+	
+	
+	
+	
+	
+	}
+
 }
 	
-}
+
 
