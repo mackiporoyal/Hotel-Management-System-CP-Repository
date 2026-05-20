c856e2542799a7a65a8af1a0ba03620d71640d78
Charles <Charles@DESKTOP-HSNALKT>
2026-05-20 23:39:23 +0800

    finishing touches

--- src/finals/UpdateStayBilling.java
+++ src/finals/UpdateStayBilling.java
@@ -64,6 +64,7 @@ public class UpdateStayBilling {
 			}
 			
 			//Header
+			
 			try {
 				List<String[]> allRows = Files.lines(Paths.get("HotelDatabase.txt")).map(line -> line.split("\\|")).collect(Collectors.toList());
 				
@@ -120,15 +121,60 @@ public class UpdateStayBilling {
 		head hd = head.values()[indexarr];
 	}
 	
+	
+	 //Changes logic
+	try {
+	List<String[]> allRows = Files.lines(Paths.get("HotelDatabase.txt")).map(line -> line.split("\\|")).collect(Collectors.toList());
+	
 	try(FileWriter writer = new FileWriter("HotelDatabase.txt", false); PrintWriter pow = new PrintWriter(writer))
 	{
+       
 		
+		
+		
+			bn[indexarr] = changes;
+			for (String[] row : allRows) {
+				if (row[0].trim().equals(booknum))
+				{
+					String changeconfirmed = String.join("|", bn);
+					pow.println(changeconfirmed);
+				}
+				else
+				{
+					String nochanges = String.join("|", row);
+					pow.println(nochanges);
+				}
 
-	
-		
+			}
 	}
 	
 	catch(IOException e)
+	{
+		System.err.println(e);
+	}
+	
+	// Updated display logic
+	int numColumns = allRows.get(0).length;
+    int[] maxWidths = new int[numColumns];
+    
+    for (String[] row : allRows) {
+        for (int i = 0; i < row.length; i++) {
+            if (row[i].length() > maxWidths[i]) {
+                maxWidths[i] = row[i].length();
+	
+            }
+        }
+    }
+	if (bn != null)
+    {
+        for (int c = 0; c < bn.length; c++) 
+        {		                	
+            System.out.printf("%-" + (maxWidths[c] + 2) + "s", bn[c]);
+        }
+        System.out.println();
+    }
+	}
+	catch (IOException e)
 	{
 		System.err.println(e);
 	}
